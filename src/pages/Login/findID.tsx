0cf8b517a5fd0cc94eed1180146cf41820a7e4b3
kkang123 <one98kr@gmail.com>
2024-05-15 16:53:38 +0900

    ✨ 아이디 & 비밀번호 찾기 초기 세팅

--- src/pages/Login/findID.tsx
+++ src/pages/Login/findID.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function findID() {
-  return <div></div>;
+export default function FindID() {
+  return (
+    <>
+      <div>
+        <label
+          className="block mb-2 text-sm font-bold text-gray-700 text-left"
+          htmlFor="email"
+        >
+          이메일
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="nickname"
+          // value={email}
+          // onChange={onChange}
+          className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          placeholder="이름을 입력해주세요."
+        />
+      </div>
+    </>
+  );
 }
