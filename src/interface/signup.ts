95fe44a632fce5b2c27fa189158e9d89a5a5984f
kkang123 <one98kr@gmail.com>
2024-05-18 23:06:46 +0900

    🔥 signup.ts -> user.ts

--- src/interface/signup.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  userId: string;
-  userName: string;
-  email: string;
-  emailId: string;
-  emailDomain: string;
-  password: string;
-  role?: string; // role은 선택적 필드입니다.
-}
--- src/interface/user.ts
+++ src/interface/user.ts
@@ -1,11 +1,8 @@
-import { Timestamp } from "firebase/firestore";
-
-export interface UserType {
-  //   id: number;
-  id: string;
-  email: string;
-  isSeller: boolean;
-  nickname: string;
-  createdAt: Timestamp;
-  updatedAt: Timestamp;
+export interface User {
+  userId: string;
+  userName: string;
+  emailId: string;
+  emailDomain: string;
+  password: string;
+  role?: string; // role은 선택적 필드입니다.
 }
