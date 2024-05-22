b66f5904356abcc3ca9d4a1cf01c40fc23de89bb
kkang123 <one98kr@gmail.com>
2024-05-22 10:58:11 +0900

    ♻️ 메인헤더 유저 상태값 적용하여 유저 값에 맞는 변수 변경

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,37 +1,26 @@
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
-import { useAuth } from "@/contexts/AuthContext";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { userState, isLoggedInState } from "@/recoil/userState";
 import { cartState } from "@/recoil/cartState";
-
-import { auth } from "@/firebase";
-import { signOut, onAuthStateChanged } from "firebase/auth";
-
 import { Button } from "@/components/ui/button";
-
 import mainlogo from "@/assets/main-logo.svg";
 import mainother from "@/assets/mainother.svg";
 import basket from "@/assets/basket-buy-cart.svg";
+import { useLogin } from "@/api/loginAPI";
 
 function MainHeader() {
-  const { isSeller } = useAuth();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [uid, setUid] = useState<string | null>(null);
+  const user = useRecoilValue(userState);
+  const isLoggedIn = useRecoilValue(isLoggedInState);
+  const [cart, setCart] = useRecoilState(cartState);
+  const [isOpen, setIsOpen] = useState(false);
+  const { logout } = useLogin();
   const navigate = useNavigate();
 
-  const [cart, setCart] = useRecoilState(cartState);
-  const resetCart = () => setCart([]);
-
-  const [isOpen, setIsOpen] = useState(false); // 드롭다운 메뉴 상태
-
-  // 드롭다운 메뉴 토글 함수
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  // localStorage에서 장바구니 정보를 호출
   useEffect(() => {
     const savedCart = localStorage.getItem("cart");
     if (savedCart) {
@@ -41,119 +30,49 @@ function MainHeader() {
 
   const uniqueProductCount = new Set(cart.map((item) => item.product.id)).size;
 
-  // 로그인 상태 확인
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setIsLoading(false);
-      if (user) {
-        setIsLoggedIn(true);
-        setUid(user.uid);
-      } else {
-        setIsLoggedIn(false);
-        setUid(null);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  const logOut = async (event: FormEvent) => {
+  const handleLogout = (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      await signOut(auth);
-      resetCart();
-      console.log("Successfully logged out");
-    } catch (error) {
-      console.error("Error during log out", error);
-    }
+    logout();
+    navigate("/");
   };
 
-  const Login = (event: FormEvent) => {
+  const handleLogin = (event: React.FormEvent) => {
     event.preventDefault();
     navigate("/login");
   };
 
-  const Home = (event: FormEvent) => {
+  const handleHome = (event: React.FormEvent) => {
     event.preventDefault();
     navigate("/");
   };
 
-  if (isLoading) {
-    return (
-      <div className="fixed px-5 py-5 top-0 left-0 right-0 flex  w-full justify-between shadow-lg  bg-white z-40 h-20">
-        <img src={mainlogo} alt="main-logo" className="w-9 h-9 pb-3 " />
-        <div className="flex">
-          {!isSeller && uid && (
-            <Link to={`/myprofile/${uid}`}>
-              <Button variant={"ghost"} size="sm">
-                마이프로필
-              </Button>
-            </Link>
-          )}
-          {isSeller && (
-            <Link to={`/productlist/${uid}`}>
-              <Button size="sm">판매자 센터</Button>
-            </Link>
-          )}
-
-          {!isSeller && (
-            <Link to={isLoggedIn && uid ? `/cart/${uid}` : "#"}>
-              <button className="">
-                <div className="relative">
-                  <img src={basket} alt="Basket" className="w-9" />
-                  {uniqueProductCount > 0 && (
-                    <span
-                      className={`text-sm text-white absolute bottom-3.5 right-${
-                        uniqueProductCount >= 10 ? "2.5" : "3.5"
-                      }`}
-                    >
-                      {uniqueProductCount}
-                    </span>
-                  )}
-                </div>
-              </button>
-            </Link>
-          )}
-          <div className=" inline-block ml-2 mr-2">
-            <Button variant="outline" size="sm" onClick={logOut}>
-              로그아웃
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  console.log(`isLoggedIn: ${isLoggedIn}, uid: ${uid}`);
-
   return (
     <>
-      <div className="fixed px-5 py-5 top-0 left-0 right-0 flex  w-full justify-between shadow-lg  bg-white z-40 h-20">
-        <button className="" onClick={Home}>
+      <div className="fixed px-5 py-5 top-0 left-0 right-0 flex w-full justify-between shadow-lg bg-white z-40 h-20">
+        <button onClick={handleHome}>
           <img src={mainlogo} alt="main-logo" className="w-9 h-9" />
         </button>
 
         <div className="hidden sm:flex">
-          {!isSeller && uid && (
-            <Link to={`/myprofile/${uid}`}>
+          {user.role !== "SELLER" && user.userId && (
+            <Link to={`/myprofile/${user.userId}`}>
               <Button variant={"ghost"} size="sm">
                 마이프로필
               </Button>
             </Link>
           )}
 
-          {isSeller && ( // isSeller가 true일 때만 '판매자 센터' 버튼을 표시합니다.
-            <Link to={`/productlist/${uid}`}>
+          {user.role === "SELLER" && (
+            <Link to={`/productlist/${user.userId}`}>
               <Button size="sm">판매자 센터</Button>
             </Link>
           )}
 
-          {/* isSeller가 false일 때만 장바구니 표시 */}
-          {!isSeller && (
-            <Link to={isLoggedIn && uid ? `/cart/${uid}` : "#"}>
-              <button className="">
+          {user.role !== "SELLER" && (
+            <Link to={isLoggedIn && user.userId ? `/cart/${user.userId}` : "#"}>
+              <button>
                 <div className="relative">
-                  <img src={basket} alt="Basket" className="w-9 pb-3 " />
+                  <img src={basket} alt="Basket" className="w-9 pb-3" />
                   {uniqueProductCount > 0 && (
                     <span
                       className={`text-sm text-white absolute bottom-3.5 right-${
@@ -167,43 +86,46 @@ function MainHeader() {
               </button>
             </Link>
           )}
-          <div className=" inline-block ml-2 mr-2">
+          <div className="inline-block ml-2 mr-2">
             {isLoggedIn ? (
-              <Button variant="outline" size="sm" onClick={logOut}>
+              <Button variant="outline" size="sm" onClick={handleLogout}>
                 로그아웃
               </Button>
             ) : (
-              <Button size="sm" onClick={Login}>
+              <Button size="sm" onClick={handleLogin}>
                 로그인
               </Button>
             )}
           </div>
         </div>
 
-        {/* sm 이하 크기에서 표시될 버튼 */}
         <button className="sm:hidden" onClick={toggleDropdown}>
-          <img src={mainother} alt="menu" className="w-10 h-10" />{" "}
-          {/* 메뉴 아이콘 이미지 경로 */}
+          <img src={mainother} alt="menu" className="w-10 h-10" />
         </button>
 
-        {/* 드롭다운 메뉴 */}
         {isOpen && (
           <div className="absolute right-5 top-20 bg-white shadow-lg rounded-md z-50 sm:hidden">
-            {!isSeller && uid && (
-              <Link to={`/myprofile/${uid}`} onClick={() => setIsOpen(false)}>
+            {user.role !== "SELLER" && user.userId && (
+              <Link
+                to={`/myprofile/${user.userId}`}
+                onClick={() => setIsOpen(false)}
+              >
                 <div className="px-4 py-2 hover:bg-gray-100">마이프로필</div>
               </Link>
             )}
 
-            {isSeller && (
-              <Link to={`/productlist/${uid}`} onClick={() => setIsOpen(false)}>
+            {user.role === "SELLER" && (
+              <Link
+                to={`/productlist/${user.userId}`}
+                onClick={() => setIsOpen(false)}
+              >
                 <div className="px-4 py-2 hover:bg-gray-100">판매자 센터</div>
               </Link>
             )}
 
-            {!isSeller && (
+            {user.role !== "SELLER" && (
               <Link
-                to={isLoggedIn && uid ? `/cart/${uid}` : "#"}
+                to={isLoggedIn && user.userId ? `/cart/${user.userId}` : "#"}
                 onClick={() => setIsOpen(false)}
               >
                 <div className="px-4 py-2 hover:bg-gray-100">장바구니</div>
@@ -212,7 +134,7 @@ function MainHeader() {
 
             <div
               className="px-4 py-2 hover:bg-gray-100"
-              onClick={isLoggedIn ? logOut : Login}
+              onClick={isLoggedIn ? handleLogout : handleLogin}
             >
               {isLoggedIn ? "로그아웃" : "로그인"}
             </div>
