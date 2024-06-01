e7bccfb9879b08c89ae8e3720422bf3fc9de668e
kkang123 <one98kr@gmail.com>
2024-06-01 20:02:03 +0900

    🔥 불필요한 코드 삭제

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
-import {
-  userState,
-  accessTokenState,
-  isLoggedInState,
-} from "@/recoil/userState";
+import { userState, isLoggedInState } from "@/recoil/userState";
 import Cookies from "js-cookie";
 
 export const useLogout = () => {
   const setUser = useSetRecoilState(userState);
-  const setAccessToken = useSetRecoilState(accessTokenState);
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
   const navigate = useNavigate();
 
@@ -18,7 +13,6 @@ export const useLogout = () => {
     localStorage.removeItem("AccessToken"); // 로컬 스토리지에서 액세스 토큰 제거
     localStorage.removeItem("user");
     Cookies.remove("REFRESH_TOKEN"); // 쿠키에서 리프레시 토큰 제거
-    setAccessToken(""); // 리코일 상태 초기화
     setIsLoggedIn(false); // 로그인 상태 초기화
     setUser({ isLoggedIn: false, userId: "", role: "" }); // 사용자 상태 초기화
     navigate("/login"); // 로그인 페이지로 리디렉션
--- src/recoil/userState.ts
+++ src/recoil/userState.ts
@@ -8,11 +8,6 @@ const getInitialUserState = () => {
     : { isLoggedIn: false, userId: "", role: "" };
 };
 
-export const accessTokenState = atom({
-  key: "accessTokenState",
-  default: localStorage.getItem("AccessToken") || "",
-});
-
 export const isLoggedInState = atom({
   key: "isLoggedInState",
   default: !!Cookies.get("REFRESH_TOKEN"), // 쿠키에 리프레시 토큰이 있는지 확인
