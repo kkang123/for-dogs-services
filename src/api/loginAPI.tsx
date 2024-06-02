311777549f847602345713688f2403bf59366295
kkang123 <one98kr@gmail.com>
2024-06-02 17:54:47 +0900

    ♻️ 리프레시 토큰 만료 시 로그아웃 구현

--- src/api/loginAPI.tsx
+++ src/api/loginAPI.tsx
@@ -1,162 +1,17 @@
-// import axios from "axios";
-// import { basicAxios } from "./axios";
-// import { useSetRecoilState } from "recoil";
-// import Cookies from "js-cookie";
-// import { useNavigate } from "react-router-dom";
-
-// import {
-//   userState,
-//   accessTokenState,
-//   isLoggedInState,
-// } from "@/recoil/userState";
-// import { Login, ServerError } from "@/interface/login";
-
-// import { useLogout } from "@/hooks/useLogout";
-
-// import Swal, { SweetAlertIcon } from "sweetalert2";
-
-// export const useLogin = () => {
-//   const setUser = useSetRecoilState(userState);
-//   const setAccessToken = useSetRecoilState(accessTokenState);
-//   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
-
-//   const { logout } = useLogout();
-
-//   const navigate = useNavigate();
-
-//   const login = async ({ userId, userPassword, userRole }: Login) => {
-//     try {
-//       const response = await basicAxios.post("/users/login", {
-//         userId,
-//         userPassword,
-//         userRole,
-//       });
-//       console.log("로그인 응답 데이터:", response.data);
-
-//       const { accessToken, refreshToken, expiresIn } = response.data.result;
-
-//       // expiresIn을 사용하여 만료 시간 계산
-//       const expiresDate = new Date(Date.now() + expiresIn * 1000);
-
-//       localStorage.setItem("AccessToken", accessToken); // 액세스 토큰을 로컬 스토리지에 저장
-//       // localStorage.setItem("expiresIn", expiresDate.toString()); // 만료 시간을 로컬 스토리지에 저장
-//       localStorage.setItem("expiresIn", expiresDate.getTime().toString()); // 만료 시간을 로컬 스토리지에 저장
-//       localStorage.setItem(
-//         "user",
-//         JSON.stringify({ isLoggedIn: true, userId, userRole })
-//       ); // 사용자 데이터를 로컬 스토리지에 저장
-//       setAccessToken(accessToken);
-
-//       Cookies.set("REFRESH_TOKEN", refreshToken, { expires: expiresDate }); // 리프레시 토큰을 쿠키에 저장
-//       setUser({ isLoggedIn: true, userId, userRole }); // 사용자 상태 업데이트
-//       setIsLoggedIn(true);
-
-//       navigate("/");
-//       console.log("Access Token:", accessToken);
-//       console.log("Refresh_Token:", refreshToken);
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         const serverResponse = error.response;
-
-//         if (
-//           serverResponse &&
-//           (serverResponse.status === 400 || serverResponse.status === 404)
-//         ) {
-//           const errorData = serverResponse.data as ServerError;
-//           console.error("Error data received from server:", errorData); // 응답 데이터 로깅
-
-//           const errorMessages: {
-//             [key: string]: {
-//               icon: SweetAlertIcon;
-//               title: string;
-//               text: string;
-//             };
-//           } = {
-//             "비밀번호가 일치하지 않습니다.": {
-//               icon: "error",
-//               title: "비밀번호 오류",
-//               text: "비밀번호가 일치하지 않습니다.",
-//             },
-//             "[password: 비밀번호를 입력해주세요.]": {
-//               icon: "error",
-//               title: "비밀번호 오류",
-//               text: "비밀번호를 입력해주세요.",
-//             },
-//             "아이디와 일치하는 회원을 찾을 수 없습니다.": {
-//               icon: "error",
-//               title: "회원정보 오류",
-//               text: "아이디와 일치하는 회원을 찾을 수 없습니다.",
-//             },
-//             "ID는 영문과 숫자만 사용할 수 있습니다.": {
-//               icon: "error",
-//               title: "아이디 오류",
-//               text: "ID는 영문과 숫자만 사용할 수 있습니다.",
-//             },
-//             "비활성화된 회원입니다. 로그인이 불가능합니다.": {
-//               icon: "error",
-//               title: "아이디 오류",
-//               text: "비활성화된 회원입니다. 로그인이 불가능합니다.",
-//             },
-//             "회원 역할이 일치하지 않습니다.": {
-//               icon: "error",
-//               title: "로그인 오류",
-//               text: "회원 역할을 확인해주세요.",
-//             },
-//           };
-
-//           if (errorData.error && errorMessages[errorData.error.message]) {
-//             Swal.fire(errorMessages[errorData.error.message]);
-//           } else {
-//             Swal.fire({
-//               icon: "error",
-//               title: "오류 발생",
-//               text: "알 수 없는 오류가 발생했습니다. 나중에 다시 시도해주세요.",
-//             });
-//           }
-//         } else {
-//           Swal.fire({
-//             icon: "error",
-//             title: "오류 발생",
-//             text: "서버 연결 실패. 나중에 다시 시도해주세요.",
-//           });
-//           console.log("무슨오류?", error);
-//         }
-//       } else {
-//         // axios 에러가 아닌 경우 처리
-//         Swal.fire({
-//           icon: "error",
-//           title: "오류 발생",
-//           text: "알 수 없는 오류가 발생했습니다. 나중에 다시 시도해주세요.",
-//         });
-//       }
-//     }
-//   };
-
-//   return { login, logout };
-// };
 import axios from "axios";
 import { basicAxios } from "./axios";
 import { useSetRecoilState } from "recoil";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
-import {
-  userState,
-  accessTokenState,
-  isLoggedInState,
-} from "@/recoil/userState";
+import { userState, isLoggedInState } from "@/recoil/userState";
 import { Login, ServerError } from "@/interface/login";
-
-import { useLogout } from "@/hooks/useLogout";
 
 import Swal, { SweetAlertIcon } from "sweetalert2";
 
 export const useLogin = () => {
   const setUser = useSetRecoilState(userState);
-  const setAccessToken = useSetRecoilState(accessTokenState);
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
-
-  const { logout } = useLogout();
 
   const navigate = useNavigate();
 
@@ -169,24 +24,39 @@ export const useLogin = () => {
       });
       console.log("로그인 응답 데이터:", response.data);
 
-      const { accessToken, refreshToken, expiresIn } = response.data.result;
+      const { accessToken, refreshToken } = response.data.result;
 
-      localStorage.setItem("AccessToken", accessToken); // 액세스 토큰을 로컬 스토리지에 저장
-      Cookies.set("REFRESH_TOKEN", refreshToken, {
-        expires: new Date(Date.now() + expiresIn * 1000),
+      // 액세스 토큰과 리프레시 토큰의 만료 시간 저장
+      const accessTokenExpiration = new Date(accessToken.expiration).getTime();
+      const refreshTokenExpiration = new Date(
+        refreshToken.expiration
+      ).getTime();
+
+      localStorage.setItem("AccessToken", accessToken.value); // 액세스 토큰을 로컬 스토리지에 저장
+      localStorage.setItem(
+        "AccessTokenExpiration",
+        accessTokenExpiration.toString()
+      );
+
+      Cookies.set("REFRESH_TOKEN", refreshToken.value, {
+        expires: new Date(refreshTokenExpiration),
       }); // 리프레시 토큰을 쿠키에 저장
+
+      // 확인을 위한 console.log 추가
+      console.log("액세스 토큰이 로컬 스토리지에 저장됨:", accessToken.value);
+      console.log("리프레시 토큰이 쿠키에 저장됨:", refreshToken.value);
+
       localStorage.setItem(
         "user",
         JSON.stringify({ isLoggedIn: true, userId, userRole })
-      ); // 사용자 데이터를 로컬 스토리지에 저장
-      setAccessToken(accessToken);
+      );
 
       setUser({ isLoggedIn: true, userId, userRole }); // 사용자 상태 업데이트
       setIsLoggedIn(true);
 
       navigate("/");
       console.log("Access Token:", accessToken);
-      console.log("Refresh_Token:", refreshToken);
+      console.log("Refresh Token:", refreshToken);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         const serverResponse = error.response;
@@ -252,7 +122,7 @@ export const useLogin = () => {
             title: "오류 발생",
             text: "서버 연결 실패. 나중에 다시 시도해주세요.",
           });
-          console.log("무슨오류?", error);
+          console.log("무슨 오류?", error);
         }
       } else {
         // axios 에러가 아닌 경우 처리
@@ -265,5 +135,5 @@ export const useLogin = () => {
     }
   };
 
-  return { login, logout };
+  return { login };
 };
--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -11,41 +11,53 @@ export const useLogout = () => {
 
   const logout = () => {
     localStorage.removeItem("AccessToken"); // 로컬 스토리지에서 액세스 토큰 제거
+    localStorage.removeItem("AccessTokenExpiration"); // 로컬 스토리지에서 액세스 토큰 만료 시간 제거
+    localStorage.removeItem("RefreshTokenExpiration"); // 로컬 스토리지에서 리프레시 토큰 만료 시간 제거
     localStorage.removeItem("user");
     Cookies.remove("REFRESH_TOKEN"); // 쿠키에서 리프레시 토큰 제거
     setIsLoggedIn(false); // 로그인 상태 초기화
     setUser({ isLoggedIn: false, userId: "", role: "" }); // 사용자 상태 초기화
     navigate("/login"); // 로그인 페이지로 리디렉션
-    console.log("로그아웃 accessToken"), localStorage.removeItem("AccessToken");
-    console.log("로그아웃 refreshToken", Cookies.remove("REFRESH_TOKEN"));
+    console.log("로그아웃: AccessToken", localStorage.getItem("AccessToken"));
+    console.log("로그아웃: RefreshToken", Cookies.get("REFRESH_TOKEN"));
   };
 
   // 토큰 만료 여부를 확인하고 만료되면 자동으로 로그아웃하는 함수
   const checkTokenExpiration = () => {
-    const accessToken = localStorage.getItem("AccessToken");
-    const refreshToken = Cookies.get("REFRESH_TOKEN");
+    const accessTokenExpiration = localStorage.getItem("AccessTokenExpiration");
+    const refreshTokenExpiration = localStorage.getItem(
+      "RefreshTokenExpiration"
+    );
 
-    console.log("AccessToken:", accessToken);
-    console.log("REFRESH_TOKEN:", refreshToken);
+    console.log("AccessTokenExpiration:", accessTokenExpiration);
+    console.log("RefreshTokenExpiration:", refreshTokenExpiration);
 
-    if (refreshToken) {
-      // 리프레시 토큰이 있으면 만료 시간을 확인하여 로그아웃 처리
-      const currentTime = Math.floor(Date.now() / 1000); // 현재 시간을 초 단위로 변환
-      if (accessToken && parseInt(accessToken) < currentTime) {
-        logout();
-      }
-    } else if (!refreshToken && accessToken) {
-      // 리프레시 토큰은 없고 액세스 토큰은 있는 경우에는 액세스 토큰 만료 후 자동 로그아웃은 진행하지 않습니다.
+    const currentTime = Date.now(); // 현재 시간을 밀리초 단위로 변환
+
+    // 리프레시 토큰 만료 확인
+    if (
+      refreshTokenExpiration &&
+      parseInt(refreshTokenExpiration) < currentTime
+    ) {
+      console.log("리프레시 토큰이 만료되었습니다.");
       logout();
-      console.log(
-        "리프레시 토큰이 만료되었습니다.. 액세스 토큰을 만료 후 로그아웃하겠습니다."
-      );
+    } else if (
+      accessTokenExpiration &&
+      parseInt(accessTokenExpiration) < currentTime
+    ) {
+      // 액세스 토큰이 만료되었지만 리프레시 토큰이 아직 유효한 경우에 대한 처리
+      console.log("액세스 토큰이 만료되었습니다.");
+      // 여기에 액세스 토큰 갱신 로직을 추가할 수 있습니다.
     }
   };
 
   // 컴포넌트가 마운트될 때마다 토큰 만료 여부를 확인합니다.
   useEffect(() => {
-    checkTokenExpiration();
+    const intervalId = setInterval(() => {
+      checkTokenExpiration();
+    }, 1000 * 60); // 매 분마다 만료 여부 확인
+
+    return () => clearInterval(intervalId); // 컴포넌트가 언마운트될 때 인터벌 정리
   }, []);
 
   return { logout };
