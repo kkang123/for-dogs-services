456a4ffa4d0519e5b5fafb92498a3af40d92a9af
kkang123 <one98kr@gmail.com>
2024-06-05 11:44:38 +0900

    ♻️ 상품 등록 api

--- src/interface/product.ts
+++ src/interface/product.ts
@@ -12,3 +12,20 @@ export interface Product {
   createdAt: Timestamp;
   updatedAt: Timestamp | FieldValue;
 }
+
+// export interface Product {
+//   description: string; // 상품 설명
+//   productName: string; // 상품명
+//   productPrice: number; // 상품 가격
+//   productQuantity: number; // 상품 수량
+//   productDescription: string; // 상품 설명
+//   productImages: string[]; // 상품 이미지 배열
+//   productCategory:
+//     | "FOOD"
+//     | "CLOTHING"
+//     | "SNACK"
+//     | "TOY"
+//     | "ACCESSORY"
+//     | "SUPPLEMENT"
+//     | "NONE"; // 상품 카테고리
+// }
