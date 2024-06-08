22bb7037ac07e83d8d8ef7b260662d8f4d00f886
kkang123 <one98kr@gmail.com>
2024-06-09 00:36:40 +0900

    ♻️ 액세스 토큰 유무로 로그인 유지는 틀린 방법인거 같아서 user 데이터로 로그인 상태 관리

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,53 @@
-import { basicAxios } from "@/api/axios";
+// import { useEffect } from "react";
+// import { useRecoilState } from "recoil";
+// import { userState, isLoggedInState } from "@/recoil/userState";
+
+// const useAuth = () => {
+//   const [user, setUser] = useRecoilState(userState);
+//   const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInState);
+
+//   //   useEffect(() => {
+//   //     const checkAuth = async () => {
+//   //       const accessToken = localStorage.getItem("AccessToken");
+//   //       const accessTokenExpiration = localStorage.getItem(
+//   //         "AccessTokenExpiration"
+//   //       );
+
+//   //   if (
+//   //     accessToken &&
+//   //     accessTokenExpiration &&
+//   //     new Date().getTime() < Number(accessTokenExpiration)
+//   //   ) {
+//   //     setIsLoggedIn(true);
+//   //   } else {
+//   // try {
+//   //   const response = await basicAxios.post("/users/refresh");
+//   //   const { accessToken } = response.data;
+//   //   const newExpiration = new Date(accessToken.expiration).getTime();
+//   //   localStorage.setItem("AccessToken", accessToken.value);
+//   //   localStorage.setItem(
+//   //     "AccessTokenExpiration",
+//   //     newExpiration.toString()
+//   //   );
+//   //   setIsLoggedIn(true);
+//   // } catch (error) {
+//   // setIsLoggedIn(false);
+//   // setUser({ isLoggedIn: false, userId: "", userRole: "" });
+//   // localStorage.removeItem("user");
+//   // localStorage.removeItem("AccessToken");
+//   // localStorage.removeItem("AccessTokenExpiration");
+//   // }
+//   //   }
+//   // };
+
+//   //     checkAuth();
+//   //   }, [setIsLoggedIn, setUser]);
+
+//   return { user, isLoggedIn };
+// };
+
+// export default useAuth;
+
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userState, isLoggedInState } from "@/recoil/userState";
@@ -8,37 +57,22 @@ const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInState);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const accessToken = localStorage.getItem("AccessToken");
-      const accessTokenExpiration = localStorage.getItem(
-        "AccessTokenExpiration"
-      );
+    const checkAuth = () => {
+      const storedUser = localStorage.getItem("user");
 
-      if (
-        accessToken &&
-        accessTokenExpiration &&
-        new Date().getTime() < Number(accessTokenExpiration)
-      ) {
-        setIsLoggedIn(true);
-      } else {
-        try {
-          const response = await basicAxios.post("/users/refresh");
-          const { accessToken } = response.data;
-          const newExpiration = new Date(accessToken.expiration).getTime();
-          localStorage.setItem("AccessToken", accessToken.value);
-          localStorage.setItem(
-            "AccessTokenExpiration",
-            newExpiration.toString()
-          );
+      if (storedUser) {
+        const parsedUser = JSON.parse(storedUser);
+        const { isLoggedIn, userId, userRole } = parsedUser;
+
+        if (isLoggedIn && userId && userRole) {
+          setUser({ isLoggedIn: true, userId, userRole });
           setIsLoggedIn(true);
-        } catch (error) {
-          setIsLoggedIn(false);
-          setUser({ isLoggedIn: false, userId: "", userRole: "" });
-          localStorage.removeItem("user");
-          localStorage.removeItem("AccessToken");
-          localStorage.removeItem("AccessTokenExpiration");
+          return;
         }
       }
+
+      setIsLoggedIn(false);
+      setUser({ isLoggedIn: false, userId: "", userRole: "" });
     };
 
     checkAuth();
