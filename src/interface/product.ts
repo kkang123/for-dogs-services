bf6a0f1608b67e939502e481c75b90236a702b17
kkang123 <one98kr@gmail.com>
2024-06-11 10:51:02 +0900

    ♻️ 상품타입 변경

--- src/interface/product.ts
+++ src/interface/product.ts
@@ -1,31 +1,17 @@
-import { Timestamp, FieldValue } from "firebase/firestore";
-
 export interface Product {
-  id: number;
-  sellerId: string;
-  productName: string;
-  productPrice: number | null;
-  productQuantity: number | null;
-  productDescription: string;
-  productCategory: string;
-  productImage: string[];
-  createdAt: Timestamp;
-  updatedAt: Timestamp | FieldValue;
+  // id: string;
+  // sellerId: string;
+  productName: string; // 상품명
+  productPrice: number; // 상품 가격
+  productQuantity: number; // 상품 수량
+  productDescription: string; // 상품 설명
+  productImages: string[]; // 상품 이미지 배열
+  productCategory:
+    | "FOOD"
+    | "CLOTHING"
+    | "SNACK"
+    | "TOY"
+    | "ACCESSORY"
+    | "SUPPLEMENT"
+    | "NONE"; // 상품 카테고리
 }
-
-// export interface Product {
-//   description: string; // 상품 설명
-//   productName: string; // 상품명
-//   productPrice: number; // 상품 가격
-//   productQuantity: number; // 상품 수량
-//   productDescription: string; // 상품 설명
-//   productImages: string[]; // 상품 이미지 배열
-//   productCategory:
-//     | "FOOD"
-//     | "CLOTHING"
-//     | "SNACK"
-//     | "TOY"
-//     | "ACCESSORY"
-//     | "SUPPLEMENT"
-//     | "NONE"; // 상품 카테고리
-// }
