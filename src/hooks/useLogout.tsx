1d293023e44d0aa374c317897b8d9405a8aa23ec
kkang123 <one98kr@gmail.com>
2024-06-26 12:47:08 +0900

    ✨ 로그아웃 api 추가

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,3 +1,4 @@
+import { basicAxios } from "@/api/axios";
 import { useSetRecoilState } from "recoil";
 import { userState, isLoggedInState } from "@/recoil/userState";
 
@@ -5,12 +6,21 @@ export const useLogout = () => {
   const setUser = useSetRecoilState(userState);
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
 
-  const logout = () => {
+  const logout = async () => {
     localStorage.removeItem("AccessToken");
     localStorage.removeItem("AccessTokenExpiration");
     localStorage.removeItem("user");
     setIsLoggedIn(false);
     setUser({ isLoggedIn: false, userId: "", role: "" });
+
+    try {
+      const response = await basicAxios.post("/users/logout", {
+        withCredentials: true,
+      });
+      console.log("로그아웃 성공 : ", response.data);
+    } catch (error) {
+      console.error("로그아웃 실패 : ", error);
+    }
   };
 
   return { logout };
