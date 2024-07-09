d6d1055ff8af97620acd580a077e66010f705807
kkang123 <one98kr@gmail.com>
2024-07-09 12:08:02 +0900

    💄 카테고리 페이지 반응형 수정

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -350,8 +350,50 @@ function Category() {
             </Button>
           </div>
 
-          <div className="flex flex-wrap justify-center border-red-200 border-4 gap-4 overflow-x-auto ">
-            <div className="flex flex-wrap justify-start w-[1270px] gap-4 border-blue-600  border-4">
+          {/* <div className="flex flex-wrap border-red-200 border-4 gap-4 overflow-x-auto ">
+            {uniqueProducts.length > 0 ? (
+              uniqueProducts.map((product: Product) => (
+                <Link
+                  key={product.productId}
+                  to={`/sellproduct/${product.productId}`}
+                  className="flex justify-center items-center"
+                >
+                  <div className="shadow border-2 rounded w-[380px] h-[380px] flex-shrink-0">
+                    {product.productImages[currentImageIndex] ? (
+                      <img
+                        className="w-full h-[300px] rounded"
+                        src={product.productImages[currentImageIndex]}
+                        alt={`Uploaded image ${currentImageIndex + 1}`}
+                      />
+                    ) : null}
+                    <div className="m-1">
+                      <div className="overflow-hidden text-overflow ellipsis whitespace-nowrap">
+                        {product.productName}
+                      </div>
+                      <div className="overflow-hidden text-overflow ellipsis whitespace-nowrap">
+                        {product.productPrice}원
+                      </div>
+                      <div className="overflow-hidden text-overflow ellipsis whitespace-nowrap font-bold">
+                        남은 수량: {product.productQuantity}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div>데이터가 없습니다.</div>
+            )}
+            <div ref={ref}></div>
+          </div>
+          {isFetchingNextPage && (
+            <div className="text-center">
+              상품을 불러오는 중입니다. 잠시만 기다려주세요!
+            </div>
+          )}
+        </div> */}
+
+          <div className="overflow-x-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 auto-rows-max border-red-200 border-4 gap-4">
               {uniqueProducts.length > 0 ? (
                 uniqueProducts.map((product: Product) => (
                   <Link
@@ -387,6 +429,7 @@ function Category() {
               <div ref={ref}></div>
             </div>
           </div>
+
           {isFetchingNextPage && (
             <div className="text-center">
               상품을 불러오는 중입니다. 잠시만 기다려주세요!
