12e3647378fae3058fa262258e52866fb91f2f3c
kkang123 <one98kr@gmail.com>
2024-09-12 14:47:37 +0900

    ✨ 주문 내역 타입 추가

--- src/interface/order.ts
+++ src/interface/order.ts
@@ -1,22 +1,14 @@
-import { Timestamp } from "firebase/firestore";
-
 export interface OrderItem {
-  product: {
-    productName: string;
-    sellerId: string;
-    productPrice: number;
-  };
-  quantity: number;
+  orderItemId: string;
+  orderProductName: string;
+  orderProductQuantity: number;
+  orderProductUnitPrice: number;
 }
 
 export interface Order {
-  id: string;
-  uid: string;
-  buyerId: string;
-  buyer_name: string;
-  amount: number;
-  item: OrderItem;
-  timestamp: Timestamp;
-  status: string;
-  groupid: string;
+  orderId: string;
+  orderStatus: string;
+  orderTotalPrice: number;
+  paymentId: string;
+  orderItems: OrderItem[];
 }
