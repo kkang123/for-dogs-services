845cf14c2fbfd584fbaafd2228eeb8871159f2bc
kkang123 <one98kr@gmail.com>
2024-09-20 16:17:28 +0900

    ✨ 판매자 프로필

--- src/pages/Profile/SellerProfile.tsx
+++ src/pages/Profile/SellerProfile.tsx
@@ -1,44 +1,40 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
-import { basicAxios } from "@/api/axios";
-
-import useDeleteUser from "@/hooks/useDeleteUser";
-import useChangePassword from "@/hooks/useChangePassword";
-
-// import { Order } from "@/interface/order";
-import { UserDetails } from "@/interface/userDetail";
-
-import SEOMetaTag from "@/components/SEOMetaTag";
-import ProductHeader from "@/components/Header/ProductHeader";
-
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 import Swal from "sweetalert2";
 
-function MyProfile() {
-  const [user, setUser] = useState<UserDetails | null>(null);
+import { basicAxios } from "@/api/axios";
+import SEOMetaTag from "@/components/SEOMetaTag";
+import ProductHeader from "@/components/Header/ProductHeader";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useUserProfile from "@/hooks/useUserProfile";
+import useDeleteUser from "@/hooks/useDeleteUser";
+import useChangePassword from "@/hooks/useChangePassword";
+import useChangeOrderStatus from "@/hooks/useOrderStateChange";
+
+import { Order } from "@/interface/order";
+
+function SellerProfile() {
+  const [selectedMenu, setSelectedMenu] = useState<string>("My Information");
+  const { user, error: userProfileError } = useUserProfile();
+  const [orderHistory, setOrderHistory] = useState<Order[]>([]);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
   const [currentPassword, setCurrentPassword] = useState<string>("");
   const [newPassword, setNewPassword] = useState<string>("");
-  const [isPasswordFieldVisible, setIsPasswordFieldVisible] =
-    useState<boolean>(false);
-
-  const { userId } = useParams<{ userId: string }>();
 
   const deleteUser = useDeleteUser();
+
   const { changePassword, isLoading, error, success } = useChangePassword();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await basicAxios.get("/users/profile");
-        setUser(response.data.result);
-      } catch (error) {
-        console.error("Failed to fetch user:", error);
-      }
-    };
-
-    fetchUser();
-  }, [userId]);
+  const { changeOrderStatus, isLoading: isChangingOrderStatus } =
+    useChangeOrderStatus();
 
   const handlePasswordChange = async () => {
     if (!currentPassword || !newPassword) {
@@ -53,111 +49,301 @@ function MyProfile() {
     await changePassword({ currentPassword, newPassword });
   };
 
-  const togglePasswordFields = () => {
-    setIsPasswordFieldVisible((prev) => !prev);
+  // 결제 상태
+  const orderStatusMap: { [key: string]: string } = {
+    AWAITING_PAYMENT: "결제 대기 중",
+    PAID: "결제 완료",
+    PAYMENT_FAILED: "결제 오류",
+    CONFIRMED: "구매 확인",
+    AWAITING_SHIPMENT: "배송 대기 중",
+    SHIPPED: "배송 중",
+    DELIVERED: "배송 완료",
+    CANCELLED: "주문 취소",
+  };
+
+  const handleStatusChange = async (orderId: string, newStatus: string) => {
+    const translatedStatus = orderStatusMap[newStatus];
+
+    const result = await Swal.fire({
+      title: "상태 변경 확인",
+      text: `상태를 "${translatedStatus}"로 변경하시겠습니까?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "예",
+      cancelButtonText: "아니오",
+    });
+
+    if (result.isConfirmed) {
+      await changeOrderStatus(orderId, { orderStatus: newStatus });
+      setOrderHistory((prevHistory) =>
+        prevHistory.map((o) =>
+          o.orderId === orderId ? { ...o, orderStatus: newStatus } : o
+        )
+      );
+    }
+  };
+
+  // 판매 내역 api
+  const fetchOrders = async () => {
+    if (startDate && endDate) {
+      try {
+        const formattedStartDate = startDate.toISOString().split("T")[0];
+
+        const endOfDay = new Date(endDate);
+        endOfDay.setHours(23, 59, 59, 999);
+        const formattedEndDate = endDate.toISOString().split("T")[0];
+
+        const response = await basicAxios.get(
+          `/orders/seller?startDate=${formattedStartDate}&endDate=${formattedEndDate}`
+        );
+
+        console.log("Fetched orders:", response.data.result);
+        setOrderHistory(response.data.result);
+      } catch (error) {
+        console.error("Failed to fetch order history:", error);
+      }
+    }
+  };
+
+  // 판매 내역 조회
+  const renderOrderHistory = () => {
+    return (
+      <div>
+        <h2 className="text-4xl pb-4">판매 내역</h2>
+
+        <div className="flex space-x-4 pb-4">
+          <div className="border border-gray-300 rounded-lg p-2 shadow-lg">
+            <label>시작 날짜: </label>
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date | null) => setStartDate(date)}
+              dateFormat="yyyy-MM-dd"
+            />
+          </div>
+          <div className="border border-gray-300 rounded-lg p-2 shadow-lg">
+            <label>종료 날짜: </label>
+            <DatePicker
+              selected={endDate}
+              onChange={(date: Date | null) => setEndDate(date)}
+              dateFormat="yyyy-MM-dd"
+            />
+          </div>
+          <Button onClick={fetchOrders}>구매 내역 조회</Button>
+        </div>
+
+        {orderHistory.length === 0 ? (
+          <p>구매 내역이 없습니다.</p>
+        ) : (
+          orderHistory.map((order) => {
+            const statusOptions = [
+              { value: "CONFIRMED", label: "구매 확인" },
+              { value: "AWAITING_SHIPMENT", label: "배송 대기 중" },
+              { value: "SHIPPED", label: "배송 중" },
+              { value: "DELIVERED", label: "배송 완료" },
+            ].filter((option) => option.value !== order.orderStatus);
+
+            return (
+              <div key={order.orderId} className="border p-4 mb-4">
+                <p className="mb-4">
+                  <strong>주문 날짜</strong> :{" "}
+                  {new Date(order.orderDate).toLocaleString("ko-KR", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                    second: "numeric",
+                    hour12: true,
+                  })}
+                </p>
+                <p>
+                  <strong>구매자 아이디</strong> : {order.buyerAccount}
+                </p>
+                <p>
+                  <strong>주문 번호</strong> : {order.orderId}
+                </p>
+                <p>
+                  <strong>주문 상태</strong> :{" "}
+                  {orderStatusMap[order.orderStatus]}
+                </p>
+
+                <div className="my-4">
+                  <label>
+                    <strong>주문 상태 변경: </strong>
+                  </label>
+                  <Select
+                    value={order.orderStatus || ""}
+                    onValueChange={(newStatus) =>
+                      handleStatusChange(order.orderId, newStatus)
+                    }
+                    disabled={isChangingOrderStatus}
+                  >
+                    <SelectTrigger className="w-40">
+                      <SelectValue>
+                        {order.orderStatus
+                          ? orderStatusMap[order.orderStatus]
+                          : "상태 선택"}
+                      </SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      {statusOptions.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <div className="my-4">
+                  <h4>주문 상품 목록:</h4>
+                  {order.orderItems.map((item, index) => (
+                    <div key={item.orderItemId} className="mb-2">
+                      <p>
+                        <strong>{index + 1}. 상품명</strong> :{" "}
+                        {item.orderProductName}
+                      </p>
+                      <p>
+                        <strong>수량</strong> : {item.orderProductQuantity}
+                      </p>
+                      <p>
+                        <strong>가격</strong> : {item.orderProductUnitPrice} 원
+                      </p>
+                    </div>
+                  ))}
+                  <p className="mt-4 text-2xl">
+                    <strong>총 금액</strong> : {order.orderTotalPrice} 원
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
+    );
+  };
+
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case "My Information":
+        return (
+          <div>
+            <h1 className="text-4xl pb-4">나의 정보</h1>
+            <Card className="m-10 border rounded-lg shadow-lg w-[500px] ">
+              <CardContent>
+                {user ? (
+                  <div className="space-y-4 m-4 p-4 ">
+                    <p>
+                      <strong>성함</strong> : {user.userName}
+                    </p>
+                    <p>
+                      <strong>생년월일</strong> : {user.userBirthDate}
+                    </p>
+                    <p>
+                      <strong>아이디</strong> : {user.userId}
+                    </p>
+                    <p>
+                      <strong>이메일</strong> : {user.userEmail}
+                    </p>
+                    <p>
+                      <strong>권한</strong> : {user.userRole}
+                    </p>
+                  </div>
+                ) : userProfileError ? (
+                  <p>{userProfileError}</p>
+                ) : (
+                  <p>회원 정보를 불러오고 있는 중입니다.</p>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        );
+      case "Purchase History":
+        return renderOrderHistory();
+      case "Change Password":
+        return (
+          <div>
+            <h2 className="text-4xl">비밀번호 수정</h2>
+            <input
+              type="password"
+              placeholder="현재 비밀번호"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              className="border p-2 mt-2"
+            />
+            <input
+              type="password"
+              placeholder="새 비밀번호"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="border p-2 m-2"
+            />
+            <Button onClick={handlePasswordChange} disabled={isLoading}>
+              비밀번호 변경하기
+            </Button>
+            {error && <p className="text-red-500">{error}</p>}
+            {success && (
+              <p className="text-green-500">비밀번호가 변경되었습니다.</p>
+            )}
+          </div>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
     <>
       <header className="h-20">
-        <ProductHeader showBackspaseButton={true} />
+        <ProductHeader showHomeButton={true} showProductManagement={true} />
         <SEOMetaTag
           title="For Dogs - MyProfile"
           description="판매자 프로필 페이지입니다."
         />
       </header>
-      <main className="mt-16 ">
-        <h1 className="text-4xl">안녕하세요. {user?.userName}님</h1>
-        <hr />
-        <h2 className="text-3xl p-2 mt-4">구매 내역</h2>
-        {/* <div className="flex-col ">
-          {Object.entries(groupedOrders).map(([groupid, orders]) => {
-            const totalAmount = orders.reduce(
-              (sum, order) =>
-                sum + (order.status === "주문 취소" ? 0 : order.amount),
-              0
-            );
-            return (
-              <div
-                key={groupid}
-                className="flex gap-2 justify-around m-2 p-2 border-2 rounded"
+
+      <main className="flex mt-10 more-element">
+        <nav className="fixed top-56 left-6 w-64 h-[calc(100vh-14rem)] p-4 z-10">
+          <ul className="space-y-4">
+            <li>
+              <Button
+                size={"lg"}
+                onClick={() => setSelectedMenu("My Information")}
               >
-                <div className="flex items-center ">
-                  구매 날짜 : {orders[0].timestamp.toDate().toLocaleString()}
-                </div>
-
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-                  {orders.map((order) => (
-                    <div key={order.id} className=" p-2">
-                      <div>주문 번호 : {order.id}</div>
-                      <div>상품 이름 : {order.item.product.productName}</div>
-                      <div>상품 갯수 : {order.item.quantity}</div>
-                      <div>
-                        판매자 :
-                        {users.find(
-                          (user) => user.uid === order.item.product.sellerId
-                        )?.nickname || "알 수 없음"}
-                      </div>
-                      <div>상품 가격: {order.item.product.productPrice}</div>
-                      <div>주문 상태 : {order.status}</div>
-                      <div>전체 가격 : {order.amount}</div>
-                      {order.status === "결제 완료" && (
-                        <Button size="sm" onClick={() => cancelOrder(order.id)}>
-                          주문 취소
-                        </Button>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                <div className="flex items-center">
-                  총 결제 가격 : {totalAmount}원
-                </div>
-              </div>
-            );
-          })}
-        </div> */}
-      </main>
-      <footer>
-        <div className="flex gap-2">
-          <div className="flex-col">
-            <Button onClick={togglePasswordFields}>
-              {isPasswordFieldVisible
-                ? "비밀번호 수정 취소"
-                : "비밀번호 수정하기"}
-            </Button>
-
-            {isPasswordFieldVisible && (
-              <div>
-                <h2 className="text-2xl">비밀번호 수정</h2>
-                <input
-                  type="password"
-                  placeholder="현재 비밀번호"
-                  value={currentPassword}
-                  onChange={(e) => setCurrentPassword(e.target.value)}
-                  className="border p-2 m-2"
-                />
-                <input
-                  type="password"
-                  placeholder="새 비밀번호"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                  className="border p-2 m-2"
-                />
-                <Button onClick={handlePasswordChange} disabled={isLoading}>
-                  비밀번호 수정하기
-                </Button>
-                {error && <p className="text-red-500">{error}</p>}
-                {success && (
-                  <p className="text-green-500">비밀번호가 변경되었습니다.</p>
-                )}
-              </div>
-            )}
-          </div>
-          <Button onClick={deleteUser}>탈퇴하기</Button>
+                나의 정보
+              </Button>
+            </li>
+            <li>
+              <Button
+                size={"lg"}
+                onClick={() => setSelectedMenu("Purchase History")}
+              >
+                판매 내역
+              </Button>
+            </li>
+            <li>
+              <Button
+                size={"lg"}
+                onClick={() => setSelectedMenu("Change Password")}
+              >
+                비밀번호 변경
+              </Button>
+            </li>
+            <li>
+              <Button size={"lg"} className="lx" onClick={deleteUser}>
+                탈퇴하기
+              </Button>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex-grow ml-64 p-4 overflow-x-auto">
+          {renderContent()}
         </div>
-      </footer>
+      </main>
+
+      <footer></footer>
     </>
   );
 }
 
-export default MyProfile;
+export default SellerProfile;
