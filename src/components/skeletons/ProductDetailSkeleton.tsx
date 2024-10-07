c92eee930fd1493ebc4f429b2b70177a63c37cd5
kkang123 <one98kr@gmail.com>
2024-10-07 15:14:54 +0900

    ♻️ 스켈레톤 UI 적용 에러 수정

--- src/components/skeletons/ProductDetailSkeleton.tsx
+++ src/components/skeletons/ProductDetailSkeleton.tsx
@@ -2,16 +2,16 @@ const ProductDetailSkeleton = () => {
   return (
     <div style={{ minWidth: "1300px" }} className="center">
       <div className="flex w-full gap-12 pt-[70px] pb-[80px] justify-center">
-        <div className="w-[580px] h-[580px] bg-gray-200 animate-pulse"></div>
+        <div className="w-[580px] h-[580px] bg-gray-200 animate-skeleton"></div>
         <div className="flex flex-col gap-20 w-[600px]">
-          <div className="w-[300px] h-[40px] bg-gray-200 animate-pulse mt-8 ml-auto"></div>
+          <div className="w-[300px] h-[40px] bg-gray-200 animate-skeleton mt-8 ml-auto"></div>
 
-          <div className=" w-[200px] h-[30px] bg-gray-200 animate-pulse mt-3 ml-auto"></div>
+          <div className=" w-[200px] h-[30px] bg-gray-200 animate-skeleton mt-3 ml-auto"></div>
 
-          <div className="w-[250px] h-[30px] bg-gray-200 animate-pulse mt-8 ml-auto"></div>
+          <div className="w-[250px] h-[30px] bg-gray-200 animate-skeleton mt-8 ml-auto"></div>
           <div className="border-b-2 mt-4"></div>
 
-          <div className="w-[100px] h-[40px] bg-gray-200 animate-pulse mt-2 ml-auto"></div>
+          <div className="w-[100px] h-[40px] bg-gray-200 animate-skeleton mt-2 ml-auto"></div>
         </div>
       </div>
 
@@ -20,7 +20,7 @@ const ProductDetailSkeleton = () => {
         className="mx-10 mt-3 border-4 border-LightBlue-500 rounded overflow-hidden"
         style={{ height: "8em" }}
       >
-        <div className="w-full h-full bg-gray-200 animate-pulse"></div>
+        <div className="w-full h-full bg-gray-200 animate-skeleton"></div>
       </div>
     </div>
   );
