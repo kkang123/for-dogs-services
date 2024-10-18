95944c7c886b6ba28a1dffeaf39f5d32109abef8
kkang123 <one98kr@gmail.com>
2024-10-18 14:42:04 +0900

    ⚡️ vitest 에 대한 커버리지 라이브러리 추가

--- vite.config.ts
+++ vite.config.ts
@@ -55,6 +55,13 @@ export default ({ mode }: { mode: string }) => {
       globals: true,
       environment: "jsdom",
       setupFiles: "./src/tests/setupTests.ts",
+      coverage: {
+        provider: "v8",
+        reporter: ["text", "json", "html"],
+        // include: ["src/**/*.ts", "src/**/*.tsx"],
+        include: ["src/pages/Login/Login.tsx"],
+        exclude: ["node_modules/", "dist/"],
+      },
     },
   });
 };
